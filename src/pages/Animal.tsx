af256f0c636f7b7ba57a997aae3a4912b6ad6c40
Shan15Dev <shansai@bluewin.ch>
2022-12-08 16:46:34 +0100

    Animal Link back to card

--- src/pages/Animal.tsx
+++ src/pages/Animal.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import AnimalPreview from "../components/AnimalPreview";
 import InformationTile from "../components/InformationTile";
+import {Link} from "react-router-dom";
 
 function Animal() {
+    document.title = "Kagaru - Zoo Zürich"
     return (
         <div>
             <div>
                 <Navbar/>
             </div>
             <div style={{paddingTop: "10pt"}}>
-                <p style={{fontFamily: "arial"}}>{"< ZURÜCK ZUR KARTE"}</p>
+                <Link to={"/maps"}>
+                    <p style={{fontFamily: "arial"}}>{"< ZURÜCK ZUR KARTE"}</p>
+                </Link>
             </div>
             <div style={{paddingTop: "20pt", paddingBottom: "20pt"}}>
                 <AnimalPreview/>
