7d97b524416e37cc986b7abdab579a614212cc7d
Shan15Dev <shansai@bluewin.ch>
2022-12-08 16:38:25 +0100

    NAv has backgroudn color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <div style={{display: "flex", alignItems: "center", justifyContent: "space-between"}}>
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            backgroundColor: "#f7c7c7",
+        }}>
             <img style={{maxWidth: "80pt", paddingBottom: "10pt"}} src={"src/assets/zoo_logo.png"}
                  alt={"Zoo Zürich logo"}/>
-            <svg style={{height: "30pt", }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            <svg style={{height: "30pt",}} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                 strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
             </svg>
 
         </div>
